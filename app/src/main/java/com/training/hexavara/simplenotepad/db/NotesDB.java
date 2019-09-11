cf91dc895f8a74c11eff3cdeb85afa63f9d7874c
najamuslim <dinatra.17051@mhs.its.ac.id>
2019-09-11 11:40:05 +0700

    first

---
+++ app/src/main/java/com/training/hexavara/simplenotepad/db/NotesDB.java
@@ -0,0 +1,28 @@
+package com.training.hexavara.simplenotepad.db;
+
+import android.content.Context;
+
+import com.training.hexavara.simplenotepad.model.Note;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+/**
+ * Created by ixi.Dv on 20/06/2018.
+ */
+@Database(entities = Note.class, version = 1)
+public abstract class NotesDB extends RoomDatabase {
+    public abstract NotesDao notesDao();
+
+    public static final String DATABSE_NAME = "notesDb";
+    private static NotesDB instance;
+
+    public static NotesDB getInstance(Context context) {
+        if (instance == null)
+            instance = Room.databaseBuilder(context, NotesDB.class, DATABSE_NAME)
+                    .allowMainThreadQueries()
+                    .build();
+        return instance;
+    }
+}
